2fbe0efabb2daa39f621023d61ade96a63e5f5a2
odykak <99960828+odykak@users.noreply.github.com>
2023-02-07 22:15:36 +0200

    Update Fiber length distribution.py

--- Fiber length distribution.py
+++ Fiber length distribution.py
@@ -1,63 +1,28 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jul 26 17:26:49 2022
-
-@author: KO
-"""
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-import seaborn as sns
 import glob
-import os
-from scipy.optimize import curve_fit
-from sklearn.linear_model import LinearRegression
-from io import StringIO
 from statistics import mean
 from statistics import stdev
 from natsort import natsorted
 from scipy.stats import norm
 
-#Appearance stuff (colors, font size etc)
-
-#My colors
-# mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=['#c3121e', '#0348a1', '#ffb01c', '#027608', '#0193b0', '#9c5300', '#949c01', '#7104b5'])
-#                                                       0sangre,   1neptune,  2pumpkin,  3clover,   4denim,    5cocoa,    6cumin,    7berry3
-
-# mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=['#e41a1c','#377eb8', '#ff7f00', '#4daf4a','#f781bf', '#a65628', '#984ea3','#999999', '#dede00'])
-
 # Nature colors
 mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=['0C5DA5', 'FF2C00', 'FF9500', '00B945', '845B97', '474747', '9e9e9e'])
-
-#Seaborn colors
-# colors=sns.color_palette("rocket",3)
 
 # Font size. Dictionary taking numerous parameters.
 plt.rcParams.update({'font.size' : 10})
 plt.rcParams['axes.axisbelow'] = True
 
-# Gets current working directory (cwd)
-# cwd=os.getcwd()
-
-# Working directory. Pick where you want to save your stuff
-wd=r"Your working directory"
-
-# # Creates a folder to store the graphs inside the cwd
- mesa= wd + '/Fibers length distribution/'
- if not os.path.exists(mesa):
-     os.makedirs(mesa)
-        
-#Data should be in folders named as in list called "ines"
-ines=['0.18 mm','0.2 mm','0.25 mm','0.5 mm','0.7 mm','1 mm']
-graph=['a','b','c','d','e','f']
+fibres=['80 μm','180 μm','200 μm','250 μm','500 μm','700 μm','1 mm']
+graph=['a','b','c','d','e','f','g']
 mean_fiber_length=[]
 dev_fiber_length=[]
 
-for index,ina in enumerate(ines):
-    #Path to data. Full path if in different folder than this script is. Otherwise *files common name part*".
-    file_list = [i for i in glob.glob("path to file/"+ina+"/Results*")]
+for index,fibre in enumerate(fibres):
+    #Path to data. Full path if in different folder than this script is. My data files all with the Result prefix in their name, were located in folder names after the average, nominal, fibre length
+    file_list = [i for i in glob.glob("Folder path/"+fibre+"/Results*")]
     #Sort in order of number appearing in front. Omit or adjust accordingly in case of different naming.
     file_list=natsorted(file_list)
     
@@ -71,23 +36,25 @@ for index,ina in enumerate(ines):
     dev_length=round(stdev(total['Length']),3)
     mean_fiber_length.append(mean_length)
     dev_fiber_length.append(dev_length)
-   
-    mu, std = norm.fit(total['Length'])
     
-    bins=13
+    # mu, std = norm.fit(total['Length'])
+    q1, q3 = total['Length'].quantile([0.75,0.25])
+    h = 2*(q1-q3)*len(total['Length'])**(-1/3)
+    bins = round((total['Length'].max()-total['Length'].min())/h)
+    
     fig = plt.figure(figsize=(3.5,2.625))
-    plt.hist(total['Length'],bins=bins,color='#1f77b4',density=True,edgecolor = 'black')
+    plt.hist(total['Length'],density=True,bins=bins,color='#1f77b4',edgecolor = 'black')#width=20,
     plt.grid(axis='y',linestyle='dashed', linewidth='0.3', color='grey',alpha=0.8)
     plt.xlabel('Fiber length (μm)')
-    plt.ylabel('Normalized fiber count')
-    plt.title(f'{ines[index]} nominal length',fontsize=12)
+    plt.ylabel('Normalized fiber count')# or count = # of fibers'
+    plt.tick_params(length=3, width=0.5)# Bot left by default True if memory serves me. All change with =False though.
+
+    mu, std = norm.fit(total['Length'])
     xmin, xmax = plt.xlim()
     x = np.linspace(xmin, xmax,100)
     p = norm.pdf(x, mu, std)
-    plt.plot(x, p, '#FF9500', linewidth=1)
+    plt.plot(x, p, '#FF9500', linewidth=2)
     print(round(mu,3),"±" ,round(std,3))
     
     ax=plt.gca()
     ax.text(0.02, 0.9, f'{graph[index]})',transform=ax.transAxes,fontsize=14, fontweight='bold')
-
-    # plt.savefig(mesa+f'{ines[index]}'+'.png', dpi=300,bbox_inches="tight",) #pad_inches=0.05
